52b53e51e96e6aec89fb686bbba432f01fbd1ac6
adwitz <adam.witzel@gmail.com>
2013-12-31 15:40:05 -0800

    Add images to tech route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,14 +6,7 @@ var app = angular.module('app', ['ngRoute',
                                  // 'app.services',
                                  // 'app.directives'
                                  ]);
-
-app.run(function($rootScope, $location, $templateCache){
-  $rootScope.go = function(location){
-    $location.path(location);
-  };
-  $templateCache.put('about.html', '<div>All about Adam</div>');
-  $templateCache.put('projects.html', '<div>Here are my projects</div>');
-}).config(function ($routeProvider) {
+app.config(function ($routeProvider) {
   $routeProvider
     .when('/about', {
       templateUrl: 'about.html',
@@ -21,7 +14,45 @@ app.run(function($rootScope, $location, $templateCache){
     .when('/projects', {
       templateUrl: 'projects.html',
     })
+    .when('/tech', {
+      templateUrl: 'tech.html',
+    })
+    .when('/where', {
+      templateUrl: 'where.html',
+    })
     .otherwise({
       redirectTo: '/'
     });
+}).run(function($rootScope, $location, $templateCache){
+  $rootScope.go = function(location){
+    if ($location.path() === location){
+      $location.path('/');
+    } else {
+      $location.path(location);
+    }
+  };
+  $templateCache.put('about.html', "<article class='descriptions'>" +
+                                      "<p>" +
+                                      "To strive, to seek, to find and to meld tech with nature, art and travel." +
+                                      "</p>" +
+                                      "<p>" +
+                                      "I cover the full-stack and deftly build client-side apps." +
+                                      "</p>" +
+                                    "</article>");
+  $templateCache.put("projects.html", "<article class='descriptions'>" +
+                                        "<p><a class='projects' href='https://github.com/caracolrec/caracol'>Caracol</a> is an open source recommendation engine for long-form articles.</p>" +
+                                      "</article>");
+  $templateCache.put("tech.html", "<article class='descriptions'>" +
+                                    "<p class='tech'>A selection of the tools I use:</p>" +
+                                    "<div class='logos javascript'></div>" +
+                                    "<div class='logos node'></div>" +
+                                    "<div class='logos angular'></div>" +
+                                    "<div class='logos backbone'></div>" +
+                                    "<div class='logos postgres'></div>" +
+                                    "<div class='logos stylus'></div>" +
+                                    // "<div class='logos mocha'></div>" +
+                                    "</article>");
+  $templateCache.put("where.html", "<article class='descriptions'>" +
+                                     "<p>Coming soon.</p>" +
+                                    "</article>");
 });
